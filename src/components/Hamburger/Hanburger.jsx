7032fed608f4c7cf5f82364269d77032b0711a4a
jmargielewski <kuba.margielewski@gmail.com>
2019-03-11 16:12:26 +0000

    added mobile menu

--- src/components/Hamburger/Hanburger.jsx
+++ src/components/Hamburger/Hanburger.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import hamburger from '../../assets/icon/hamburger.svg';
+import './hamburger.css';
 
 const Hamburger = () => (
   <img src={hamburger} className="hamburger" alt="hamburger" />
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,28 +13,31 @@ class Header extends PureComponent {
     return (
       <header className="site-header">
         <h1 className="site-heading">Multi Level Menu</h1>
-        <ul className="site-menu">
-          {menuStructure
-            && menuStructure.map(menuItem => (
-              <li className="site-menu--item" key={menuItem.key}>
-                <span className="site-menu--value">{menuItem.label}</span>
-                {menuItem.child && (
-                  <ul className="site-submenu">
-                    {menuItem.child.map(({ key, label, content }) => (
-                      <li
-                        key={key}
-                        className="site-submenu--item"
-                        onClick={() => this.onClick(content)}
-                        onKeyPress={this.onKeyPress}
-                      >
-                        <span className="site-submenu--value">{label}</span>
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-        </ul>
+        <span className="wrap-menu">
+          <Hamburger />
+          <ul className="site-menu">
+            {menuStructure
+              && menuStructure.map(menuItem => (
+                <li className="site-menu--item" key={menuItem.key}>
+                  <span className="site-menu--value">{menuItem.label}</span>
+                  {menuItem.child && (
+                    <ul className="site-submenu">
+                      {menuItem.child.map(({ key, label, content }) => (
+                        <li
+                          key={key}
+                          className="site-submenu--item"
+                          onClick={() => this.onClick(content)}
+                          onKeyPress={this.onKeyPress}
+                        >
+                          <span className="site-submenu--value">{label}</span>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+          </ul>
+        </span>
       </header>
     );
   }
